08725c571884e7f05e0ee65a2a1a05c8a5693eec
T-Witmer <troykwitmer@gmail.com>
2024-05-18 15:55:26 -0400

    more

--- nvim/lua/troy/plugins/bufferline.lua
+++ nvim/lua/troy/plugins/bufferline.lua
@@ -1,10 +1,5 @@
-return{
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
-    opts = {
-        options = {
-        mode = "tabs",
-        },
-    },
-}
+return {
+    'akinsho/bufferline.nvim',
+    version = "*", 
+    dependencies = 'nvim-tree/nvim-web-devicons'
+ }
