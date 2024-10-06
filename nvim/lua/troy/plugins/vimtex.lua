8e9cac40e878e51480d955d13402ffcac2a7b833
T-Witmer <troykwitmer@gmail.com>
2024-10-06 13:56:29 -0400

    zathura config stolen from @tallguyjenks

--- nvim/lua/troy/plugins/vimtex.lua
+++ nvim/lua/troy/plugins/vimtex.lua
@@ -1,9 +1,9 @@
 return {
-    "lervag/vimtex",
-    lazy = false,
-    config = function()
-        vim.cmd("let g:vimtex_view_general_viewer = 'zathura'")
-        vim.cmd("let g:vimtex_view_zathura_options = '-reuse-instance'")
-        vim.cmd("let g:tex_flavor = 'latex'")
-    end
+	"lervag/vimtex",
+	lazy = false,
+	config = function()
+		vim.cmd("let g:tex_flavor = 'latex'")
+		vim.cmd("let g:vimtex_view_general_viewer = 'zathura'")
+		vim.cmd("let g:vimtex_view_zathura_options = '-reuse-instance'")
+	end,
 }
