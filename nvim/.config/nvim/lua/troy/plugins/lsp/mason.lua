8b7802cdd5ce80d1d6b083d97951b4278e43c313
chroy2 <troykwitmer@gmail.com>
2025-06-24 13:46:50 -0400

    refresh

--- nvim/.config/nvim/lua/troy/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/troy/plugins/lsp/mason.lua
@@ -1,36 +1,36 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
 
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "→",
-					pakage_uninstalled = "✗",
-				},
-			},
-		})
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"clangd",
-				"gopls",
-				"pyright",
-				"ruff",
-				"svelte",
-				"graphql",
-				"emmet_ls",
-				"lua_ls",
-				"eslint",
-			},
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "→",
+          pakage_uninstalled = "✗",
+        },
+      },
+    })
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "clangd",
+        "gopls",
+        "pyright",
+        "ruff",
+        "svelte",
+        "graphql",
+        "emmet_ls",
+        "lua_ls",
+        "eslint",
+        "ts_ls",
+      },
 
-			automatic_installation = true,
-		})
-	end,
+      automatic_installation = true,
+    })
+  end,
 }
