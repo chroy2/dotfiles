79f0bbe8dd300f4c90961d1799c0ddf196dfd09c
chroy2 <troykwitmer@gmail.com>
2025-08-14 19:17:20 -0400

    fresh nvim start

--- nvim/.config/nvim/lua/troy/core/init.lua
+++
@@ -1,2 +0,0 @@
-require("troy.core.options")
-require("troy.core.keymaps")
--- nvim/.config/nvim/lua/troy/plugins/vimtex.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"lervag/vimtex",
-	lazy = false,
-	config = function()
-		vim.cmd("let g:tex_flavor = 'latex'")
-		vim.cmd("let g:vimtex_view_method = 'zathura'")
-	end,
-}
