1ff61f77735fed078e34240079b31859c7f0665e
chroy2 <troykwitmer@gmail.com>
2025-08-14 20:37:11 -0400

    bare bones

---
+++ nvim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,15 @@
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.mouse = 'a'
+vim.autoindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true 
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.list = true
+vim.opt.listchars = { tab = "··", space = "·" }
