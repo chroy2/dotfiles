00a26228a9b28619a8a1f742ec24f3ad11a9a920
chroy2 <troykwitmer@gmail.com>
2026-03-14 17:17:47 -0400

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,9 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 zoxide init fish | source
 
+
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+    #     smth smth
+end
