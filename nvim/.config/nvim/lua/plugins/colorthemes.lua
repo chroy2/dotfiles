5c88dfbd2464348ad943e90543bf09af57c9e8c8
chroy2 <troykwitmer@gmail.com>
2026-03-15 18:44:28 -0400

    gruvbox not working

--- nvim/.config/nvim/lua/plugins/colorthemes.lua
+++ nvim/.config/nvim/lua/plugins/colorthemes.lua
@@ -1,7 +1,7 @@
 return {
 	{ "catppuccin/nvim", name = "catppuccin" },
 	{ "folke/tokyonight.nvim" },
-	{ "ellisonleao/gruvbox.nvim" },
+	{ "ellisonleao/gruvbox.nvim" }, -- currently doesnt work directly with ghostty (dark, light material etc not included in colorscheme name)
 	{ "dracula/vim", name = "dracula" },
 	{ "rebelot/kanagawa.nvim" },
 	{ "nyoom-engineering/oxocarbon.nvim" },
