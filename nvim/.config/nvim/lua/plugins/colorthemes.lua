4ed7827fd899ac7fddd3d5f326a2f2e5d196d55e
chroy2 <troykwitmer@gmail.com>
2026-03-15 20:10:56 -0400

    tmux and gruvbox material

--- nvim/.config/nvim/lua/plugins/colorthemes.lua
+++ nvim/.config/nvim/lua/plugins/colorthemes.lua
@@ -2,6 +2,7 @@ return {
 	{ "catppuccin/nvim", name = "catppuccin" },
 	{ "folke/tokyonight.nvim" },
 	{ "ellisonleao/gruvbox.nvim" }, -- currently doesnt work directly with ghostty (dark, light material etc not included in colorscheme name)
+	{ "f4z3r/gruvbox-material.nvim", name = "gruvbox-material" },
 	{ "dracula/vim", name = "dracula" },
 	{ "rebelot/kanagawa.nvim" },
 	{ "nyoom-engineering/oxocarbon.nvim" },
