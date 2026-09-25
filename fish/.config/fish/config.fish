72a952c82ab3bbf1ce8bc90d8c10121c5e893bd9
chroy2 <troykwitmer@gmail.com>
2026-09-25 18:34:52 -0400

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
+# The following lines were added by Docker Desktop to add commands to your PATH.
+export PATH="$PATH:/Users/troywitmer/.docker/bin"
+# End of Docker Desktop section.
+
 if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
     source /usr/share/cachyos-fish-config/cachyos-config.fish
 end
@@ -9,3 +13,7 @@ zoxide init fish | source
 function fish_greeting
     #     smth smth
 end
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
+export PATH="$HOME/.local/bin:$PATH"
